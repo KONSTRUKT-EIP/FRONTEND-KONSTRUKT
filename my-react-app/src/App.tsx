840520af7b82b171f560cce8b4261dcd7513d3fb
terra894 <thomas.ly-bingler@epitech.eu>
2026-03-02 16:50:45 +0100

    chore: fix some coding style

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import Login from './Components/Login/Login';
-import HomePage from './Components/HomePage/HomePage';
+// import HomePage from './Components/HomePage/HomePage';
 
 const App: React.FC = () => {
     // Pour afficher HomePage, remplacer <Login /> par <HomePage />
